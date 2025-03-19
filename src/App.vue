9d3dc2c092060c26661a3cef8fb6856d3f8cf8d4
Гусь Гитарист <kenkarlson@mail.ru>
2025-03-19 10:03:37 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Составление диеты</h1>
+
+      <!-- Форма для ввода данных -->
+      <form @submit.prevent="addFoodItem">
+        <label for="foodItem">Продукт:</label>
+        <input type="text" id="foodItem" v-model="newFoodItem" placeholder="Введите продукт" required>
+
+        <label for="calories">Калории:</label>
+        <input type="number" id="calories" v-model="newCalories" placeholder="Введите калории" required>
+
+        <button type="submit">Добавить в диету</button>
+      </form>
+
+      <!-- Список добавленных продуктов -->
+      <h2>Ваша диета:</h2>
+      <ul>
+        <FoodItem v-for="(item, index) in dietItems" :key="index" :item="item" @remove="removeFoodItem(index)" />
+      </ul>
+
+      <!-- Общее количество калорий -->
+      <p>Общее количество калорий: <span class="total-calories">{{ totalCalories }}</span></p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
